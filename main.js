90f2611e6e799ad894a875830218790f296099ab
ElsatheFrozen <81809525+ElsatheFrozen@users.noreply.github.com>
2021-11-09 10:21:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
 status_song1 = "";
+status_song2 = "";
 
 function preload()
 {
@@ -47,6 +48,19 @@ function draw() {
             document.getElementById("song_name").innerHTML = "Song Name = " + "Peter Pan";
         }
     }
+
+    status_song2 = song2.isPlaying();
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(status_song2 == false)
+        {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song Name = " + "Harry Potter Theme Song";
+        }
+    }
 }
 
 function gotPoses(results)
@@ -54,8 +68,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score; 
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
         
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
